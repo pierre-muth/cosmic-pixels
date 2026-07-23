db4c3ea4aed552f7fddca2061e354995612e9155
Muth <muth.inc@free.fr>
2026-07-23 10:29:34 +0200

    adding comments
    Removed unnecessary comments and improved code clarity.

--- pic_code_v3/main.c
+++ pic_code_v3/main.c
@@ -1,36 +1,26 @@
 /**
   Generated Main Source File
-
   Company:
     Microchip Technology Inc.
-
   File Name:
     main.c
-
   Summary:
     This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
   Description:
     This header file provides implementations for driver APIs for all modules selected in the GUI.
     Generation Information :
         Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.8
         Device            :  PIC16F18346
         Driver Version    :  2.00
-*/
-
-/*
     (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
     Subject to your compliance with these terms, you may use Microchip software and any 
     derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
     license terms applicable to your use of third party software (including open source software) that 
     may accompany Microchip software.
-    
     THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
     EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
     IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
     FOR A PARTICULAR PURPOSE.
-    
     IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
     INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
     WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
@@ -46,7 +36,7 @@
 const uint8_t GRADIENT_RED[]   = {0,   4,  50, 100, 255, 255, 255, 255, 255, 255};
 const uint8_t GRADIENT_GREEN[] = {0,   0,   0,   0,  20,  90, 120, 180, 190, 190};
 const uint8_t GRADIENT_BLUE[]  = {0,   2,   0,   0,   0,   0,   0,  50,  80,  80};
-const double GRADIENT_STEP_NUMBER = 9.0; // number of interval in the gradient points
+const double GRADIENT_STEP_NUMBER = 9.0; // number of interval in the gradient array
 const double GRADIENT_STEP_SIZE = 1.0 / GRADIENT_STEP_NUMBER;
 const double DECAY_GRADIENT = 0.005;
 bool interrupt_single_event = 0;
@@ -70,7 +60,7 @@ void main(void) {
     // enable SPI module
     SSP1CON1bits.SSPEN = 1;
     
-    // set DAC output
+    // set DAC output to max (4.096V)
     DAC1_SetOutput(0b11111);
     
     // variables
@@ -78,16 +68,16 @@ void main(void) {
     double localGradientVal = 0.0;      // value in between two gradient points
     double redStart, greenStart, blueStart;
     double redStop, greenStop, blueStop;
-    uint8_t red, green, blue;
+    uint8_t red, green, blue;	// final color values
     uint8_t gradientStartIndex = 0;
     uint16_t loopCount = 0;
-    uint16_t event_counter = 0;
+    uint16_t event_counter = 0;  // Number of event per ~16s, used to adjust DAC output
     
     // main loop
     while (1) {
         __delay_ms(10);
         
-        // forced events for debug
+        // For debug: forced events 
 //        if (loopCount % 400 == 0){
 //            interrupt_single_event = true;
 //            interrupt_cosmic_event = false;
@@ -98,22 +88,21 @@ void main(void) {
 //        }
         loopCount++;
         
-        // do we have a coincidence event
+        // do we have a coincidence event or just a single event    
         if (interrupt_single_event && interrupt_cosmic_event) {
             cosmic_brightness = 1.0;
             interrupt_single_event = false;
             interrupt_cosmic_event = false;
             event_counter++;
-        // or a single event    
         } else if (interrupt_single_event && !interrupt_cosmic_event) {
-            PWM5_LoadDutyValue(500);    // 0-999
+            PWM5_LoadDutyValue(500);    // for a flash on MainLed [0-999]
             interrupt_single_event = false;
             event_counter++;
         } else {
             PWM5_LoadDutyValue(0);
         }
         
-        // decay brightness value
+        // decay the brightness value
         if (cosmic_brightness > 1.0) cosmic_brightness = 1.0;
         if (cosmic_brightness >= DECAY_GRADIENT) cosmic_brightness -= DECAY_GRADIENT;
         else cosmic_brightness = 0;
@@ -121,7 +110,6 @@ void main(void) {
         // RGB gradient calculation
         gradientStartIndex = (uint8_t)(cosmic_brightness * GRADIENT_STEP_NUMBER);
         localGradientVal = (cosmic_brightness - (gradientStartIndex*GRADIENT_STEP_SIZE))/GRADIENT_STEP_SIZE;
-        
         redStart    = GRADIENT_RED[gradientStartIndex];
         greenStart  = GRADIENT_GREEN[gradientStartIndex];
         blueStart   = GRADIENT_BLUE[gradientStartIndex];
@@ -146,19 +134,19 @@ void main(void) {
         SPI1_ExchangeByte(0xFF);
         SPI1_ExchangeByte(0xFF);
         
-        // timer0 flag (16 seconds), check the event count and adjust DAC output
+        // Timer0 flag (~16 seconds). Check the event count rate and adjust DAC output accordingly 
         if (TMR0IF){
-            if (event_counter > 40 && DAC1_GetOutput() < 0b11111) {
-                DAC1_SetOutput(DAC1_GetOutput() +1);
+            if (event_counter > 40 && DAC1_GetOutput() < 0b11111) {  // too much events, if DAC not maxed already:
+                DAC1_SetOutput(DAC1_GetOutput() +1);  // increase the detection threshold
             }
-            if (event_counter < 3 && DAC1_GetOutput() > 0b00111) {
-                DAC1_SetOutput(DAC1_GetOutput() -1);
+            if (event_counter < 3 && DAC1_GetOutput() > 0b00111) {  // too few events, if DAC not too low already:
+                DAC1_SetOutput(DAC1_GetOutput() -1);  // decrease the detection threshold
             }
             
             TMR0IF = 0;
             event_counter = 0;
             
-            // generate a square signal on Sync_out pin
+            // for debug: generate a square signal on Sync_out pin
             Sync_out_Toggle();
         }
     }
